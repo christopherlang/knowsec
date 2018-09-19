6847a9d7b1469ac1cc8a1758a173560fc796f34d
Christopher Lang <chlang206@gmail.com>
2018-09-19 00:20:03 -0400

    Test code, showing how to use

--- knowsec/knowsec.py
+++ knowsec/knowsec.py
@@ -9,8 +9,7 @@ import pytz
 import time
 import random
 
-# os.chdir('C:/Users/Christopher Lang/Documents/projects/knowsec')
-# os.chdir('/Users/christopherlang/Documents/projects/knowsec')
+
 CONFIG = dict()
 with open('../config/data_config.yaml', 'r') as f:
     CONFIG['data'] = yaml.load(f)
@@ -29,24 +28,9 @@ comp_table = comp_table[0].append(comp_table[1]).append(comp_table[2])
 
 db.update_company(comp_table)
 
-tsdata = dfu.TimeSeriesData()
+stock_data = dfu.AlphaAdvantage()
 
+tmp = stock_data.retrieve_data('AMD', output='full')
+tmp = dfu.standardize_stock_series(tmp)
 
-for i in range(100):
-    rec_dt = dt(random.sample(range(1000, 2020), 1)[0],
-                random.sample(range(1, 13), 1)[0],
-                random.sample(range(1, 29), 1)[0])
-    rec = {
-        'Symbol': comp_table['Symbol'][i],
-        'Datetime': pytz.timezone('UTC').localize(rec_dt),
-        'open': random.uniform(30, 100),
-        'high': random.uniform(30, 100),
-        'close': random.uniform(30, 100),
-        'volume': 104583729
-    }
-
-    db.append_record('eod_stockprices', rec)
-
-    print(i)
-
-db.append_record('eod_stockprices', rec)
+db.bulk_insert_records('eod_stockprices', tmp)
