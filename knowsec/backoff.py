c9f38edf3d88372c4ba528d0baf19da7c743a874
christopherLang <chlang206@gmail.com>
2018-09-25 11:59:22 -0400

    Added Jittered Exponential backoff

--- knowsec/backoff.py
+++ knowsec/backoff.py
@@ -2,7 +2,7 @@ import time
 import random
 
 
-def exponential_backoff(max_backoff, verbose=True):
+def exponential_backoff(max_wait, verbose=True):
     """Backoff a call exponentially
 
     Returns a function that when called, will execute `time.sleep` to stop
@@ -10,7 +10,6 @@ def exponential_backoff(max_backoff, verbose=True):
 
     Time spent sleeping follows an exponential backoff algorithm, as dictated
     by Google. The time spent waiting follows the formula:
-
         wait time = min(2 ** n + random milliseconds, maximum backoff)
             Where 'n' is the number of retries
             Where 'random milliseconds' is a value less than or equal to 1000
@@ -31,7 +30,7 @@ def exponential_backoff(max_backoff, verbose=True):
 
     Parameters
     ----------
-    max_backoff : int or float
+    max_wait : int or float
         The maximum time to wait, in seconds
     verbose : bool
         If waiting, should it print how many seconds it is doing so
@@ -53,7 +52,7 @@ def exponential_backoff(max_backoff, verbose=True):
         ----------
         should_wait : bool
             If `True`, the function will sleep, following an exponential
-            backoff scheme, up to the set `max_backoff`. If `False` then the
+            backoff scheme, up to the set `max_wait`. If `False` then the
             internal count of retries is reset to zero
 
         Returns
@@ -71,7 +70,7 @@ def exponential_backoff(max_backoff, verbose=True):
         else:
             wait_time = (2 ** backoff_ntries)
             wait_time += float(random.randint(0, 1000) / 1000)
-            wait_time = min(wait_time, max_backoff)
+            wait_time = min(wait_time, max_wait)
 
             backoff_ntries += 1
 
@@ -85,14 +84,91 @@ def exponential_backoff(max_backoff, verbose=True):
     return backoff_fun
 
 
-def linear_backoff(max_backoff, initial_wait=1, step=1, verbose=True):
+def jittered_backoff(max_wait, base_wait=1, verbose=True):
+    """Backoff a call exponentially with decorrelated jitter
+
+    Time spent sleeping follows an exponential backoff algorithm with jitter,
+    as dictated by AWS
+
+    >>> bckoff = jittered_backoff(32)  # max wait time set to 32 seconds
+    >>> bckoff()
+    Sleeping for 1.01 seconds
+    {'ntries': 1, 'wait_time': 1.011439601853178}
+    >>> bckoff()
+    Sleeping for 2.34 seconds
+    {'ntries': 2, 'wait_time': 2.336598367699084}
+    >>> bckoff(False)
+    {'ntries': 0, 'wait_time': 0}
+    >>> bckoff()
+    Sleeping for 1.20 seconds
+    {'ntries': 1, 'wait_time': 1.203851331708007}
+
+    Parameters
+    ----------
+    max_wait : int or float
+        The maximum time to wait, in seconds
+    base_wait : int or float
+        The minimum time to wait, in seconds
+    verbose : bool
+        If waiting, should it print how many seconds it is doing so
+
+    Returns
+    -------
+    function
+        A function that can be executed to wait for a certain amount of time
+        It has one parameter `should_wait`. If `True`, then the function will
+        sleep at a determined time. If `False`, then it will reset the internal
+        count
+    """
+
+    backoff_ntries = 0
+
+    def backoff_fun(should_wait=True):
+        """Exponential Backoff closure
+
+        Parameters
+        ----------
+        should_wait : bool
+            If `True`, the function will sleep, following an exponential
+            backoff scheme, up to the set `max_wait`. If `False` then the
+            internal count of retries is reset to zero
+
+        Returns
+        -------
+        dict
+            A dictionary with two keys: 'ntries', specifying how many the
+            number of retries, and 'wait_time', specifying how many seconds
+            it had just slept on
+        """
+        nonlocal backoff_ntries
+
+        if should_wait is False:
+            backoff_ntries = 0
+            wait_time = 0
+        else:
+            wait_time = min(max_wait, base_wait * 2 ** backoff_ntries)
+            wait_time = wait_time / 2 + random.uniform(0, wait_time / 2)
+            wait_time = min(max_wait, random.uniform(base_wait, wait_time * 3))
+
+            backoff_ntries += 1
+
+            if verbose is True:
+                print(f'Sleeping for {wait_time:.2f} seconds')
+
+            time.sleep(wait_time)
+
+        return {'ntries': backoff_ntries, 'wait_time': wait_time}
+
+    return backoff_fun
+
+
+def linear_backoff(max_wait, initial_wait=1, step=1, verbose=True):
     """Backoff a call linearly
 
     Returns a function that when called, will execute `time.sleep` to stop
     the line of execution
 
     Time spent sleeping follows a constant, step-wise increase:
-
         wait time = min(initial_wait + (n * step), maximum backoff)
             Where 'n' is the number of retries
             Where 'step' is the linear increase of time, in seconds
@@ -115,7 +191,7 @@ def linear_backoff(max_backoff, initial_wait=1, step=1, verbose=True):
 
     Parameters
     ----------
-    max_backoff : int or float
+    max_wait : int or float
         The maximum time to wait, in seconds
     initial_wait : int or float
         The starting wait time in seconds
@@ -141,7 +217,7 @@ def linear_backoff(max_backoff, initial_wait=1, step=1, verbose=True):
         ----------
         should_wait : bool
             If `True`, the function will sleep, following an linear
-            backoff scheme, up to the set `max_backoff`. If `False` then the
+            backoff scheme, up to the set `max_wait`. If `False` then the
             internal count of retries is reset to zero
 
         Returns
@@ -152,14 +228,14 @@ def linear_backoff(max_backoff, initial_wait=1, step=1, verbose=True):
             it had just slept on
         """
         nonlocal backoff_ntries
-        nonlocal max_backoff
+        nonlocal max_wait
 
         if should_wait is False:
             backoff_ntries = 0
             wait_time = 0
         else:
             wait_time = min(initial_wait + (backoff_ntries * step),
-                            max_backoff)
+                            max_wait)
 
             backoff_ntries += 1
 
@@ -178,7 +254,6 @@ def constant_backoff(backoff, verbose=True):
 
     Returns a function that when called, will execute `time.sleep` to stop
     the line of execution
-
     The time spent sleeping is always the same
 
     >>> bckoff = constant_backoff(2)  # Wait for 2 seconds per call
@@ -213,7 +288,7 @@ def constant_backoff(backoff, verbose=True):
         ----------
         should_wait : bool
             If `True`, the function will sleep, following an linear
-            backoff scheme, up to the set `max_backoff`. If `False` then the
+            backoff scheme, up to the set `max_wait`. If `False` then the
             internal count of retries is reset to zero
 
         Returns
