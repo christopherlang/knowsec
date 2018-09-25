cb906d023ccec968c6b8ef9f4a68e4a6e5e630a9
Christopher Lang <chlang206@gmail.com>
2018-09-25 01:15:59 -0400

    Updated database code. Still in flux

--- knowsec/knowsec.py
+++ knowsec/knowsec.py
@@ -1,13 +1,12 @@
 import os
 # import sys
+import backoff
 import yaml
 import dfu
 import database
-import importlib
-from datetime import datetime as dt
-import pytz
-import time
-import random
+import tqdm
+import pandas
+import datetime as dt
 
 
 CONFIG = dict()
@@ -16,8 +15,13 @@ with open('../config/data_config.yaml', 'r') as f:
 
 db = database.StockDB('../db/storage.db')
 
-db.delete_table('eod_stockprices')
-db.create_table('eod_stockprices')
+try:
+    db.delete_table('eod_stockprices')
+
+except database.NoTableError:
+    pass
+
+db.create_all_tables()
 
 # Perform company table update
 comp_table = list()
@@ -28,9 +32,78 @@ comp_table = comp_table[0].append(comp_table[1]).append(comp_table[2])
 
 db.update_company(comp_table)
 
-stock_data = dfu.AlphaAdvantage()
+ds = dfu.AlphaVantage()
 
-tmp = stock_data.retrieve_data('AMD', output='full')
-tmp = dfu.standardize_stock_series(tmp)
+stock_update_log = db.retrieve_security_log()
+pdidx = pandas.IndexSlice
 
-db.bulk_insert_records('eod_stockprices', tmp)
+symbol_list = set([i[0] for i in comp_table.index.get_values()])
+symbol_list = [i for i in symbol_list if i != '']
+
+records_update = list()
+
+backoff_fun = backoff.exponential_backoff(120, False)  # max wait is 2 minutes
+pbar = tqdm.tqdm(symbol_list, ncols=100)
+for sym in pbar:
+    sym_in_log = False
+
+    try:
+        sym_update_log = stock_update_log.loc[[pdidx[sym]], :]
+        sym_in_log = True
+
+    except KeyError:
+        sym_update_log = None
+
+    from_dt = None
+    if sym_update_log is None:
+        output_type = 'full'
+
+    else:
+        try:
+            from_dt = sym_update_log.loc[pdidx[sym], 'maximum_datetime']
+            from_dt = from_dt.tolist()[0] + dt.timedelta(days=1)
+
+        except KeyError:
+            output_type = 'full'
+
+    while True:
+        series = None
+
+        try:
+            if from_dt is None or output_type == 'full':
+                series = ds.retrieve_data(sym, output='full')
+
+            else:
+                series = ds.retrieve_latest(sym, from_dt)
+
+            backoff_fun(False)
+            break
+
+        except (dfu.InvalidCallError, dfu.GeneralCallError):
+            break
+
+        except (dfu.RateLimitError, dfu.requests.HTTPError):
+            time_sleep = backoff_fun(True)['wait_time']
+            pbar.write(f'Sleeping for {time_sleep:.2f} seconds')
+
+    if series is not None:
+        series = dfu.standardize_stock_series(series)
+        db.bulk_insert_records('eod_stockprices', series)
+
+        series_dt = [i[1] for i in series.index.get_values()]
+
+        series_log = {
+            'Symbol': sym,
+            'minimum_datetime': min(series_dt).to_pydatetime(),
+            'maximum_datetime': max(series_dt).to_pydatetime(),
+            'update_dt': dt.datetime.utcnow()
+        }
+
+        if sym_in_log is True:
+            # TODO implement update method in database.StockDB
+            pass
+        else:
+            series_log['minimum_datetime'] = min(series_dt).to_pydatetime()
+            db.insert_record('eod_stockprices_update_log', series_log)
+
+    db.commit()
