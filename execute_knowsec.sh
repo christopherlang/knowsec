5d7574babdac5a20b022bbeed9b534095190ce8d
Christopher Lang <chlang206@gmail.com>
2019-08-06 03:35:21 +0000

    Fixing execution script

--- execute_knowsec.sh
+++ execute_knowsec.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 cd /home/ubuntu/knowsec/knowsec
-source /home/ubuntu/anaconda3/bin/activate knowsec
-echo $PWD
+source /home/ubuntu/anaconda3/bin/activate
+conda activate knowsec
+python knowsec.py
